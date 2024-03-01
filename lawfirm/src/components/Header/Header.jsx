851ec3afc433393377d47e1fb95756d92ea7f7df
tanujas <tanujas@pharmarack.com>
2024-03-01 11:06:41 +0530

    test header

---
+++ lawfirm/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import '../../assests/css/styles.css';
+import { StyledEngineProvider } from '@mui/material/styles';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      header
+    </div>
+  );
+}
+
+export default App;
